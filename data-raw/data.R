aa46d1efc028087f921c50a7f6232afc7f7ca139
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-02 17:01:09 +0000

    udpate data.R

--- data-raw/data.R
+++ data-raw/data.R
@@ -10,4 +10,9 @@ coverageData <- get_liberia_data(id = "liberiaCoverageRound2",
                                  rep = TRUE,
                                  rep.name = "child_repeat")
 
-
+villageData <- get_liberia_data(id = "liberiaVillageFormRound2",
+                                 username = "cadnihead",
+                                 password = "kEv-hAB-Arb-6Cn",
+                                 start = "2019-08-27",
+                                 end = Sys.Date(),
+                                 filename = "villageData")
