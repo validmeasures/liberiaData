fa7ca060785a674a817637b6f2f7ad0739fcf262
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-02 17:00:49 +0000

    create merge_data.R

---
+++ R/02-merge_data.R
@@ -0,0 +1,24 @@
+################################################################################
+#
+#' Function to merge main ODK form with repeats
+#'
+#' @param x A list of data.frames produced by `get_liberia_data` after
+#'   retreiving data from ODK server.
+#' @param rep.name A character value specifying name of repeat form to merge
+#'   with main form.
+#'
+#' @return A data.frame with rows equal to repeat data merged with main form
+#'
+#' @examples
+#' \dontrun{
+#' merge_repeats(x = coverageData, rep.name = "child_repeat)
+#' }
+#'
+#' @export
+#'
+#
+################################################################################
+
+merge_repeats <- function(x, rep.name) {
+  merge(x[[1]], x[[rep.name]], by.x = "KEY", by.y = "PARENT_KEY")
+}
