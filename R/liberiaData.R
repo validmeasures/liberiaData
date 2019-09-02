32a1bd0c4d41e889362abded3a3c17e51dbf993c
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-02 09:42:35 +0000

    update liberiaData.R

---
+++ R/liberiaData.R
@@ -0,0 +1,12 @@
+################################################################################
+#
+#' Liberia Coverage Survey Data Handler
+#'
+#' @docType package
+#' @name liberiaData
+#' @importFrom odkr renameODK get_briefcase pull_remote export_data
+#' @importFrom utils read.csv
+#'
+#
+################################################################################
+"_PACKAGE"
