499896cd7a7f270bc87fd6df33c1df1ab1fa1c8b
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-03 04:21:07 +0000

    create dataset documentation

---
+++ R/data.R
@@ -0,0 +1,41 @@
+################################################################################
+#
+#' Liberia Coverage Survey Data Round 2
+#'
+#' @format A data.frame with 133 columns
+#'
+#' * `sdate` - Date of data collection
+#' * `cid` - County identifier
+#' * `county` - County name
+#' * `did` - District identifier
+#'
+#' @source Liberia Coverage Survey Round 2 - 27 August to 30 September 2019
+#'
+#
+################################################################################
+"coverageData2"
+
+
+################################################################################
+#
+#' Liberia Coverage Survey Village Data Round 2
+#'
+#' @format A data.frame with 11 columns
+#'
+#' * `cid` - County identifier
+#' * `county` - County name
+#' * `did` - District identifier
+#' * `eid` - Enumerator identifier
+#' * `lat` - Provided latitude coordinate
+#' * `lon` - Provided longitude coordinate
+#' * `pop` - Population
+#' * `coords.Latitude` - Recorded latitude coordinate
+#' * `coords.Longitude` - Recorded longitude coordinate
+#' * `coords.Altitude` - Recorded altitude
+#' * `coords.Accuracy` - Accuracy of coordinates recording
+#'
+#' @source Liberia Coverage Survey Round 2 - 27 August to 30 September 2019
+#'
+#
+################################################################################
+"villageData2"
