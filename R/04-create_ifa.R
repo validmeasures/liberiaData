5a4cca57569542d208b2658a5a10644b8d3a2df4
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-03 09:11:11 +0000

    update create_ifa

--- R/04-create_ifa.R
+++ R/04-create_ifa.R
@@ -1,6 +1,6 @@
 ################################################################################
 #
-#' Function to create iron-folic acid coverage indicators
+#' Function to recode iron-folic acid coverage indicators
 #'
 #' @param df A coverage data.frame collected for the Liberia Coverage Survey
 #' @param core.columns A vector of variable names to include in resulting
@@ -28,21 +28,28 @@ calculate_ifa <- function(df, core.columns = c("cid", "did", "eid", "m2")) {
   ifa3 <- bbw::recode(ifa3, "2=0;99=0")
   ## Reasons: Health centre ran out
   ifa3a <- as.numeric(df$ifa3a.1)
+  ifa3a[is.na(df$ifa1)] <- NA
   ## Reasons: Took too long to get tablets
   ifa3b <- as.numeric(df$ifa3a.2)
+  ifa3b[is.na(df$ifa1)] <- NA
   ## Reasons: Too expensive
   ifa3c <- as.numeric(df$ifa3a.3)
+  ifa3c[is.na(df$ifa1)] <- NA
   ## Take IFA?
   ifa4 <- as.numeric(df$ifa4)
   ifa4 <- bbw::recode(ifa4, "2=0;99=0")
   ## Reasons: Concerns about side effects
   ifa4a <- as.numeric(df$ifa4a.1)
+  ifa4a[is.na(df$ifa1)] <- NA
   ## Reasons: Don't need it
   ifa4b <- as.numeric(df$ifa4a.2)
+  ifa4b[is.na(df$ifa1)] <- NA
   ## Reasons: I was told not to take it
   ifa4c <- as.numeric(df$ifa4a.3)
+  ifa4c[is.na(df$ifa1)] <- NA
   ## Reasons: I don't think it helps
   ifa4d <- as.numeric(df$ifa4a.4)
+  ifa4d[is.na(df$ifa1)] <- NA
   ## How many days?
   ifa5 <- as.numeric(df$ifa5)
   ## Create IFA data.frame
