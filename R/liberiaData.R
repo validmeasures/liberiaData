423e58efc966fdf0001bd23d2edf299b6704530c
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-03 08:09:16 +0000

    update liberiaData package documentation

--- R/liberiaData.R
+++ R/liberiaData.R
@@ -9,7 +9,11 @@
 #' @importFrom odkr renameODK get_briefcase pull_remote export_data
 #' @importFrom utils read.csv
 #' @importFrom bbw recode
+#' @importFrom stringr str_detect
 #'
 #
 ################################################################################
 "_PACKAGE"
+
+## quiets concerns of R CMD check re: the string that appear in recode_others
+if(getRversion() >= "2.15.1")  utils::globalVariables("string")
