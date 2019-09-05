9b3c37db5979007fce9387b13a6033bf80b7eb7b
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-05 10:37:49 +0000

    update datasets

--- data-raw/data.R
+++ data-raw/data.R
@@ -136,11 +136,16 @@ childID[!is.na(coverageData[[2]]$c1)] <- 1:length(childID[!is.na(coverageData[[2
 coverageData[[2]] <- data.frame(childID, coverageData[[2]])
 coverageData2 <- coverageData[[2]]
 
-columns2 <- c("childID", "sex", "card", "know_dob", "dob", "age_report", "age",
-              "age_check", "muac1", "muac_screen", "oedema1", "oedema_screen",
-              "oedema_check", "muac", "sam", "muac2", "oedema2",
-              "oedema_check2", "muac1a", "sam1a", "muac3", "oedema3",
-              "oedema_check3", "muac1b", "sam1b", "nut_status", "cov_status",
+columns2 <- c("childID", "card", "know_dob", "dob", "age_report",
+              "sex", "sex1", "sex2",
+              "age", "age_check", "age1", "age_check1", "age2", "age_check2",
+              "muac1", "muac_screen", "muac2", "muac3",
+              "weight1", "weight_screen", "weight2", "weight3",
+              "height1", "height_screen", "height2", "height3",
+              "oedema1", "oedema_screen", "oedema_check",
+              "oedema2", "oedema_check2", "oedema3", "oedema_check3",
+              "sam", "sam1a", "sam1b",
+              "nut_status", "cov_status",
               "in.", "recovering", "out", "q1", "q1a.1", "q1a.2", "q1a.3",
               "q1a.4", "q1a.5", "q1a.6", "q2", "q3", "q4.q4a", "q4.q4b",
               "q4.q4c", "q4.q4d", "q4.q4e", "q4.q4f", "q4.q4g", "q4.q4h",
@@ -153,39 +158,7 @@ coverageData <- merge_repeats(x = coverageData, rep.name = "child_repeat")
 
 coverageData1.r2 <- coverageData1[ , columns1]
 coverageData2.r2 <- coverageData2[ , columns2]
-coverageData.r2 <- coverageData[ , c("motherID", "sdate", "cid", "county", "did",
-                                     "eid", "consent2", "m2", "ch", "mcheck1",
-                                     "ifa1", "ifa2", "ifa3", "ifa3a.1", "ifa3a.2",
-                                     "ifa3a.3", "ifa3a.88", "ifa3a_other", "ifa4",
-                                     "ifa4a.1", "ifa4a.2", "ifa4a.3", "ifa4a.4",
-                                     "ifa4a.88", "ifa4a_other","ifa5", "icf1",
-                                     "icf2", "icf2a.1", "icf2a.2", "icf2a.3",
-                                     "icf2a.4", "icf2a.88", "icf2a_other", "icf3",
-                                     "c2", "cage", "csex", "mnp1", "mnp2",
-                                     "mnp2a.1", "mnp2a.2", "mnp2a.3", "mnp2a.4",
-                                     "mnp2a.5", "mnp2a.88", "mnp2a_other", "mnp3",
-                                     "mnp3a.1", "mnp3a.2", "mnp3a.3", "mnp3a.4",
-                                     "mnp3a.5", "mnp3a.6", "mnp3a.7", "mnp3a.8",
-                                     "mnp3a.9", "mnp3a.88", "mnp3a_other", "mnp4",
-                                     "vit1", "vit1a.1", "vit1a.2", "vit1a.3",
-                                     "vit1a.4", "vit1a.88", "vit1a_other", "vit2",
-                                     "vit3",
-                                     "childID", "sex", "card", "know_dob", "dob",
-                                     "age_report", "age", "age_check", "muac1",
-                                     "muac_screen", "oedema1", "oedema_screen",
-                                     "oedema_check", "muac", "sam", "muac2",
-                                     "oedema2", "oedema_check2", "muac1a",
-                                     "sam1a", "muac3", "oedema3", "oedema_check3",
-                                     "muac1b", "sam1b", "nut_status", "cov_status",
-                                     "in.", "recovering", "out", "q1", "q1a.1",
-                                     "q1a.2", "q1a.3", "q1a.4", "q1a.5", "q1a.6",
-                                     "q2", "q3", "q4.q4a", "q4.q4b", "q4.q4c",
-                                     "q4.q4d", "q4.q4e", "q4.q4f", "q4.q4g",
-                                     "q4.q4h", "q4.q4i", "q4.q4j", "q4.q4k",
-                                     "q4.q4l", "q4.q4m", "q4.q4n", "q4.q4o",
-                                     "q4.q4p", "q4.q4q", "q4.q4r", "q4.q4s", "q4t",
-                                     "q5", "q6.q6a", "q6.q6b", "q6.q6c", "q6.q6d",
-                                     "q6e")]
+coverageData.r2 <- coverageData[ , c(columns1, columns2)]
 
 ########################### Create internal datasets ###########################
 
