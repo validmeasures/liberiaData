f40a168d33a96ff222434e6320c28ac54c9533bc
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-06 05:30:44 +0000

    create maps

---
+++ data-raw/maps.R
@@ -0,0 +1,24 @@
+##
+if(!require(remotes)) install.packages("remotes")
+if(!require(rgdal)) install.packages("rgdal")
+if(!require(rgeos)) install.packages("rgeos")
+if(!require(raster)) install.packages("raster")
+##
+if(!require(liberia)) remotes::install_github("validmeasures/liberia")
+if(!require(liberiaData)) remotes::install_github("validmeasures/liberiaData")
+
+##
+gmHexGrid <- readOGR(dsn = "data-raw/maps/coveragePolyGM", layer = "coveragePolyGM", verbose = FALSE)
+gmHexGrid@data <- subset(gmHexGrid@data, select = county)
+writeOGR(gmHexGrid, dsn = "data-raw/maps/gmHexGrid.gpkg", layer = "Monrovia", driver = "GPKG",
+         overwrite_layer = TRUE, delete_dsn = FALSE)
+##
+usethis::use_data(gmHexGrid, overwrite = TRUE)
+
+##
+gbHexGrid <- readOGR(dsn = "data-raw/maps/coveragePolyGB", layer = "coveragePolyGB", verbose = FALSE)
+gbHexGrid@data <- subset(gbHexGrid@data, select = county)
+writeOGR(gbHexGrid, dsn = "data-raw/maps/gbHexGrid.gpkg", layer = "Grand Bassa", driver = "GPKG",
+         overwrite_layer = TRUE, delete_dsn = FALSE)
+##
+usethis::use_data(gbHexGrid, overwrite = TRUE)
