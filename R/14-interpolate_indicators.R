f875472306e04b593a023f2169f7f42ea14bbd55
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-07 08:51:53 +0000

    update interpolate_indicators

--- R/14-interpolate_indicators.R
+++ R/14-interpolate_indicators.R
@@ -40,14 +40,36 @@ interpolate_indicators <- function(indicator = c("ifaDF", "iycfDF",
     ##
     currentDF <- get(i)
     ##
+    ## Check if i is anthroDF
+    if(i == "anthroDF") {
+      core.columns <- c(core.columns, "age", "sex", "position", "flag")
+    }
+    ## Check if i is cmamDF
+    if(i == "cmamDF") {
+      core.columns <- c("spid", "cin", "cout", "rin")
+    }
+    ##
     currentDFresults <- data.frame(matrix(nrow = length(hexgrid),
                                           ncol = length(names(currentDF)[!names(currentDF) %in% core.columns])))
     ##
     names(currentDFresults) <- names(currentDF)[!names(currentDF) %in% core.columns]
-    ##
-    currentSP <- create_sp(df = currentDF,
-                           coords = coords[ , c("EFEACODE", "lon", "lat")],
-                           proj = sp::CRS(sp::proj4string(hexgrid)))
+    ## check if i is cmamDF
+    if(i == "cmamDF") {
+      temp <- aggregate(x = coords[ , c("lon", "lat")],
+                        by = list(coords$spid),
+                        FUN = mean)
+      names(temp) <- c("spid", "lon", "lat")
+      currentSP <- create_sp(df = currentDF,
+                             coords = temp,
+                             by.x = "spid", by.y = "spid",
+                             proj = sp::CRS(sp::proj4string(hexgrid)))
+    } else {
+      ##
+      currentSP <- create_sp(df = currentDF,
+                             coords = coords[ , c("EFEACODE", "lon", "lat")],
+                             by.x = "eid", by.y = "EFEACODE",
+                             proj = sp::CRS(sp::proj4string(hexgrid)))
+    }
     ##
     for(j in names(currentDF)[!names(currentDF) %in% core.columns]) {
       currentIndicator <- currentSP[!is.na(currentSP[[j]]), ]
