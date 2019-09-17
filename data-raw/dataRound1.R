06890d768615576494000414375652a6ef933f8a
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-17 05:51:11 +0100

    add dataRound1.R

--- data-raw/dataRound1.R
+++ data-raw/dataRound1.R
@@ -4,9 +4,9 @@ options(stringsAsFactors = FALSE)
 
 ifaDF.r1 <- read.csv("data-raw/dataRound1/ifaBoot.csv")
 
-ifaInt.r1 <- read.csv("data-raw/")
 ifaBoot.r1 <- read.csv("data-raw/dataRound1/ifaEst.csv")
 
 ## Restructure ifaBoot.r1 to match structure of ifaBoot
 
 x <- ifaBoot.r1[ifaBoot.r1$county == "Monrovia", ]
+y <- ifaBoot.r1[ifaBoot.r1$county == "Grand Bassa", ]
